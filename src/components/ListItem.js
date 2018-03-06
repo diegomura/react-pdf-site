1f0743a26f5da4ae63dc09a1a6b39b17581a4634
Diego Muracciole <diegomuracciole@gmail.com>
2018-03-06 13:23:06 -0300

    Refactor ListItem

---
+++ src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ListItem = styled.li`
+  color: #3E3E3E;
+  font-size: 16px;
+  line-height: 36px;
+  font-family: 'Source Sans Pro';
+`;
+
+export default ListItem;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Markdown from 'react-markdown';
 import styled from 'styled-components';
+
+// components
+import List from '../components/List';
 import Title from '../components/Title';
 import Heading from '../components/Heading';
+import ListItem from '../components/ListItem';
 import CodeBlock from '../components/CodeBlock';
+
+// assets
 import CornerGraphics from '../static/images/corner-graphics.png';
 import DocumentGraphic from '../static/images/document-graphic.png';
 import index from '../index.md';
@@ -11,6 +17,8 @@ import index from '../index.md';
 const renderers = {
   heading: Heading,
   code: CodeBlock,
+  list: List,
+  listItem: ListItem,
 };
 
 const CornerGraphicsImage = styled.img`
